c3607b44d4b1dc17a05325f561d2b65d1b2d61c0
NarsheS <shadownightmaresnw@gmail.com>
2026-06-23 16:20:57 -0300

    comments on main.py scraper

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import json
 
 url = "https://www.olx.com.br/imoveis/venda"
 
-html = get_page(url)
+# Acessa a página simulando um usuário no navegador
+html = get_page(url) 
 
+# Obtém os dados dentro da html, em um script de id __NEXT_DATA__
 data = get_next_data(html)
 
+# Obtém o alvo (anúncios) dentro do script
 ads = get_ads(data)
 
+# Formatando os dados para json
 parsed = [parse_ad(ad) for ad in ads]
 
 print(json.dumps(parsed, indent=2, ensure_ascii=False))
